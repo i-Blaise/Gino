4d819b3e01971ab7df07e8403526721be5f04da3
Your Name <menniablaise@yahoo.com>
2023-06-17 13:00:57 +0000

    show errors

--- winners/index.php
+++ winners/index.php
@@ -2,6 +2,10 @@
 
 require_once('../ClassLibraries/MainClass.php');
 $mainPlug = new mainClass();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 
 <!DOCTYPE html>
